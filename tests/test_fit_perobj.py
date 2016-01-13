1d80d7bdc5c7dd3488bf08dda52ca151363bda52
steven-murray <steven.murray@curtin.edu.au>
2016-01-13 12:14:05 +0800

    changed API to match new FitPerObj object

--- tests/test_fit_perobj.py
+++ tests/test_fit_perobj.py
@@ -1,5 +1,5 @@
 import numpy as np
-from mrpy.fit_perobj import fit_perobj_opt
+from mrpy.fit_perobj import PerObjFit
 from mrpy.core import dndm, A_rhoc, _getnorm
 from mrpy.stats import TGGD
 from nose.tools import nottest
@@ -8,9 +8,9 @@ np.random.seed(42)
 
 def test_s0():
     t = TGGD(scale=1e14,a=-1.8,b=1.0,xmin=1e12)
-    r = t.rvs(1e5)
-
-    res,po = fit_perobj_opt(r, hs0=14, alpha0=-1.8, beta0=1.0, s=0)
+    r = t.rvs(3e4)
+    FitObj = PerObjFit(r,weight_scale=0)
+    res,po = FitObj.run_downhill(hs0=14, alpha0=-1.8, beta0=1.0)
 
     print res.x
     assert res.success
@@ -19,23 +19,11 @@ def test_s0():
 
 def test_s1():
     t = TGGD(scale=1e14,a=-1.8,b=1.0,xmin=1e12)
-    r = t.rvs(4e5)
-    print np.log10(r).min(), np.log10(r).max()
-    res,po = fit_perobj_opt(r, hs0=14, alpha0=-1.8, beta0=1.0, s=1,
-                            alpha_bounds=(-1.95,-1.7), hs_bounds=(10,16),
-                            beta_bounds=(0.8,1.2))
+    r = t.rvs(5e5)
+    FitObj = PerObjFit(r,weight_scale=1.0,alpha_bounds=(-1.95,-1.7), hs_bounds=(10,16),
+                                 beta_bounds=(0.8,1.2))
+    res,po = FitObj.run_downhill(hs0=14, alpha0=-1.8, beta0=1.0)
 
     print res.x
     assert res.success
     assert np.all(np.isclose(res.x,[14.0,-1.8,1.0],rtol=5e-2))
-
-@nottest
-def test_bounds():
-    t = TGGD(scale=1e14, a=-1.8, b=1.0, xmin=1e12)
-    r = t.rvs(1e5)
-
-    res,po = fit_perobj_opt(r, hs0=14, alpha0=-1.8, beta0=1.0, s=0,
-                            bounds=None)
-
-    print res.x
-    assert res.success
