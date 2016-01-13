1613c66b2ccd2a6ef2337f46972ba8f97ae8fe54
steven-murray <steven.murray@curtin.edu.au>
2016-01-13 12:11:08 +0800

    a couple of tests for nbar and rhobar with general m

--- tests/test_core.py
+++ tests/test_core.py
@@ -81,9 +81,21 @@ class TestMRP(object):
         print self.mrp.nbar
         assert np.isclose(self.mrp.nbar,1.0)
 
+    def test_nbar_general_m(self):
+        logm = np.linspace(11,15,100)
+        mrp = core.MRP(logm,14.0,-1.9,0.75,log_mmin=10.0)
+        assert np.isclose(mrp.nbar,1.0)
+
     def test_arhoc(self):
         logm = np.linspace(0,16,1000)
         mrp = core.MRP(logm,14.0,-1.85,0.75,norm="rhoc")
+        print mrp.rhobar/(0.3*2.7755e11)
+        assert np.isclose(mrp.rhobar,0.3*2.7755e11,rtol=0.05)
+
+
+    def test_arhoc_general_m(self):
+        logm = np.linspace(10,16,1000)
+        mrp = core.MRP(logm,14.0,-1.85,0.75,norm="rhoc",log_mmin=0.0)
         print mrp.rhobar/(0.3*2.7755e11)
         assert np.isclose(mrp.rhobar,0.3*2.7755e11,rtol=0.05)
 
